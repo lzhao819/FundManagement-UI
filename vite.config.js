63b40bfd50c80117a59660fe120de3795a6dd204
Lu Zhao <lzhao819@usc.edu>
2021-08-27 15:35:05 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import vue from '@vitejs/plugin-vue'
 
+module.exports = {
+    publicPath: '/FundManagement-UI/'
+}
+
 export default {
     base: './',
     plugins: [vue()],
     optimizeDeps: {
         include: ['schart.js']
-    },
-    publicPath: "/FundManagement-UI/"
+    }
+    //publicPath: ""
 }
